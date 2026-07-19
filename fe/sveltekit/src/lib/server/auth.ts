6c289ede4633fc87cb1e4ffc084a7c71274b8900
Lé <elcasaboxi@gmail.com>
2026-07-19 15:18:33 +0800

    fe db prelim

--- fe/sveltekit/src/lib/server/auth.ts
+++ fe/sveltekit/src/lib/server/auth.ts
@@ -8,15 +8,46 @@ import { db } from '$lib/server/db';
 export const auth = betterAuth({
 	baseURL: env.FE_URL,
 	secret: env.BETTER_AUTH_SECRET,
-	database: drizzleAdapter(db, { provider: 'pg' }),
+	database: drizzleAdapter(db, { provider: `pg` }),
 	emailAndPassword: { enabled: true },
 	socialProviders: {
 		github: {
-			clientId: env.GITHUB_CLIENT_ID,
-			clientSecret: env.GITHUB_CLIENT_SECRET
+			clientId: env.GITHUB_CLIENT_ID as string,
+			clientSecret: env.GITHUB_CLIENT_SECRET as string
+		},
+		google: {
+			clientId: env.GOOGLE_CLIENT_ID as string,
+			clientSecret: env.GOOGLE_CLIENT_SECRET as string
 		}
 	},
 	plugins: [
 		sveltekitCookies(getRequestEvent) // make sure this is the last plugin in the array
-	]
+	],
+	user: {
+		fields: {
+			createdAt: `created`,
+			updatedAt: `updated`,
+		}
+	},
+	session: {
+		fields: {
+			createdAt: `created`,
+			updatedAt: `updated`,
+		}
+	},
+	account: {
+		fields: {
+			accessTokenExpiresAt: `accessTokenExpires`,
+			createdAt: `created`,
+			refreshTokenExpiresAt: `refreshTokenExpires`,
+			updatedAt: `updated`,
+		}
+	},
+	verification: {
+		fields: {
+			createdAt: `created`,
+			expiresAt: `expires`,
+			updatedAt: `updated`,
+		}
+	}
 });
